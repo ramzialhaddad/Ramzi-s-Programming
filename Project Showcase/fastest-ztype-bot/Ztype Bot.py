50fcb0737489c6d5a6b40d2852589703cad5ec9b
Ramzi Al Haddad <ramzialhaddadtm@gmail.com>
2018-10-09 23:04:38 +0400

    Added a Project that I forgot to add before hand

---
+++ Project Showcase/fastest-ztype-bot/Ztype Bot.py
@@ -0,0 +1,8 @@
+#Copyright (c) Ramzi Al Haddad 2002-2017
+import pyautogui as pag
+import time
+
+time.sleep(5)
+
+while True:
+	pag.typewrite(['e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z','e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z','e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z','e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z','e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z','e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z','e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z'],interval=0.0001)
